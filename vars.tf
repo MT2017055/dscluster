940b823e2047bd72d3c548cdf2b7acd7278f3e70
Praveengostu <praveengostu@gmail.com>
2019-11-06 12:22:35 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,9 @@
-variable "cluster" {
+variable "cluster_1" {
   description = "name of cluster"
 }
 variable "region" {
+  description = "region of cluster"
+}
+variable "zone" {
   description = "region of cluster"
 }
