2d17b7e466e0c80b8842702d0fbcfc234a1f83aa
Praveengostu <praveengostu@gmail.com>
2019-11-07 16:11:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,3 @@ data "ibm_container_cluster" "cluster_data" {
  cluster_name_id             = "${var.cluster1}"
  region                      = "us-south"
 }
-
-data "ibm_container_cluster" "cluster_abc" {
- cluster_name_id             = "${var.cluster2}"
- region                      = "us-south"
-}
-
-data "ibm_container_cluster" "cluster_def" {
- cluster_name_id             = "test"
- region                      = "us-south"
-}
-
