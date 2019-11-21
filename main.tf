46366f67df771f52ffd44bce04e16dafce706d66
Praveengostu <praveengostu@gmail.com>
2019-11-21 22:39:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-data "ibm_container_cluster" "cluster_data" {
- cluster_name_id             = "${var.cluster1}"
- region                      = "us-south"
+
+
+data ibm_schematics_workspace resources {
+  workspace_id = "${var.resources_workspace_id}"
+}
+
+data ibm_schematics_output resources {
+  workspace_id = "${var.resources_workspace_id}"
+  template_id  = "${element(data.ibm_schematics_workspace.resources.template_id, 0)}"
 }
